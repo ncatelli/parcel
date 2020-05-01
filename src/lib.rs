6b027d6178fa8eadb4cf45f7b26299e3d33987a5
Nate Catelli <ncatelli@packetfire.org>
2020-05-01 17:56:46 -0500

    Stub out a minimal parcel implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,23 @@
 #[cfg(test)]
 mod tests;
 
-pub type ParseResult<'a, Input, Output> = Result<(&'a Input, Output), &'a Input>;
+pub type ParseResult<'a, Input, Output> = Result<(Input, Output), Input>;
 
 pub trait Parser<'a, Input, Output> {
-    fn parse(&self, input: &'a Input) -> ParseResult<'a, Input, Output>;
-
-    fn map<F, NewOutput>(self, map_fn: F) -> BoxedParser<'a, Input, NewOutput>
-    where
-        Self: Sized + 'a,
-        Output: 'a,
-        Input: Sized + 'a,
-        NewOutput: 'a,
-        F: Fn(Output) -> NewOutput + 'a,
-    {
-        BoxedParser::new(map(self, map_fn))
-    }
-
-    fn and_then<F, NextParser, NewOutput>(self, f: F) -> BoxedParser<'a, Input, NewOutput>
-    where
-        Self: Sized + 'a,
-        Output: 'a,
-        Input: Sized + 'a,
-        NewOutput: 'a,
-        NextParser: Parser<'a, Input, NewOutput> + 'a,
-        F: Fn(Output) -> NextParser + 'a,
-    {
-        BoxedParser::new(and_then(self, f))
-    }
-
-    fn or<P>(self, thunk_to_parser: impl Fn() -> P + 'a) -> BoxedParser<'a, Input, Output>
-    where
-        Self: Sized + 'a,
-        Input: 'a,
-        Output: 'a,
-        P: Parser<'a, Input, Output> + 'a,
-    {
-        BoxedParser::new(either(self, thunk_to_parser))
-    }
+    fn parse(&self, input: Input) -> ParseResult<'a, Input, Output>;
 }
 
 impl<'a, F, Input, Output> Parser<'a, Input, Output> for F
 where
     Input: 'a,
-    F: Fn(&'a Input) -> ParseResult<Input, Output>,
+    F: Fn(Input) -> ParseResult<'a, Input, Output>,
 {
-    fn parse(&self, input: &'a Input) -> ParseResult<'a, Input, Output> {
+    fn parse(&self, input: Input) -> ParseResult<'a, Input, Output> {
         self(input)
     }
 }
 
-pub struct BoxedParser<'a, Input, Output> {
-    parser: Box<dyn Parser<'a, Input, Output> + 'a>,
-}
-
-impl<'a, Input, Output> BoxedParser<'a, Input, Output> {
-    fn new<P>(parser: P) -> Self
-    where
-        P: Parser<'a, Input, Output> + 'a,
-    {
-        BoxedParser {
-            parser: Box::new(parser),
-        }
-    }
-}
-
-impl<'a, Input, Output> Parser<'a, Input, Output> for BoxedParser<'a, Input, Output> {
-    fn parse(&self, input: &'a Input) -> ParseResult<'a, Input, Output> {
-        self.parser.parse(input)
-    }
-}
-
-fn either<'a, P1, P2, A: 'a, B>(
-    parser1: P1,
-    thunk_to_parser: impl Fn() -> P2,
-) -> impl Parser<'a, A, B>
-where
-    P1: Parser<'a, A, B>,
-    P2: Parser<'a, A, B>,
-{
-    move |input| match parser1.parse(input) {
-        ok @ Ok(_) => ok,
-        Err(_) => thunk_to_parser().parse(input),
-    }
-}
-
-fn map<'a, P, F, A: 'a, B, C>(parser: P, map_fn: F) -> impl Parser<'a, A, C>
+pub fn map<'a, P, F, A: 'a, B, C>(parser: P, map_fn: F) -> impl Parser<'a, A, C>
 where
     P: Parser<'a, A, B>,
     F: Fn(B) -> C + 'a,
@@ -95,82 +27,4 @@ where
             .parse(input)
             .map(|(next_input, result)| (next_input, map_fn(result)))
     }
-}
-
-fn and_then<'a, P, F, A: 'a, B, C, NextP>(parser: P, f: F) -> impl Parser<'a, A, C>
-where
-    P: Parser<'a, A, B>,
-    NextP: Parser<'a, A, C>,
-    F: Fn(B) -> NextP,
-{
-    move |input| match parser.parse(input) {
-        Ok((next_input, result)) => f(result).parse(next_input),
-        Err(err) => Err(err),
-    }
-}
-
-#[allow(dead_code)]
-fn take_while<'a, P, A: 'a, B>(parser: P) -> impl Parser<'a, A, Vec<B>>
-where
-    P: Parser<'a, A, B>,
-{
-    move |mut input| {
-        let mut result_acc: Vec<B> = Vec::new();
-        while let Ok((next_input, result)) = parser.parse(input) {
-            input = next_input;
-            result_acc.push(result);
-        }
-
-        Ok((input, result_acc))
-    }
-}
-
-#[allow(dead_code)]
-fn join<'a, P1, P2, A: 'a, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, A, (R1, R2)>
-where
-    P1: Parser<'a, A, R1>,
-    P2: Parser<'a, A, R2>,
-{
-    move |input| {
-        parser1.parse(input).and_then(|(next_input, result1)| {
-            parser2
-                .parse(next_input)
-                .map(|(last_input, result2)| (last_input, (result1, result2)))
-        })
-    }
-}
-
-#[allow(dead_code)]
-fn left<'a, P1: 'a, P2: 'a, A: 'a, R1: 'a, R2: 'a>(
-    parser1: P1,
-    parser2: P2,
-) -> impl Parser<'a, A, R1>
-where
-    P1: Parser<'a, A, R1>,
-    P2: Parser<'a, A, R2>,
-{
-    join(parser1, parser2).map(|(left, _right)| left)
-}
-
-#[allow(dead_code)]
-fn right<'a, P1: 'a, P2: 'a, A: 'a, R1: 'a, R2: 'a>(
-    parser1: P1,
-    parser2: P2,
-) -> impl Parser<'a, A, R2>
-where
-    P1: Parser<'a, A, R1>,
-    P2: Parser<'a, A, R2>,
-{
-    join(parser1, parser2).map(|(_left, right)| right)
-}
-
-#[allow(dead_code)]
-fn unzip<A, B>(pair: Vec<(A, B)>) -> (Vec<A>, Vec<B>) {
-    let mut left_vec: Vec<A> = vec![];
-    let mut right_vec: Vec<B> = vec![];
-    pair.into_iter().for_each(|(left, right)| {
-        left_vec.push(left);
-        right_vec.push(right);
-    });
-    (left_vec, right_vec)
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,4 +1,25 @@
+use crate::{map, Parser};
+
+fn match_char<'a>(expected: char) -> impl Parser<'a, &'a [char], char> {
+    move |input: &'a [char]| match input.get(0) {
+        Some(next) if *next == expected => Ok((&input[1..], next.clone())),
+        _ => Err(input),
+    }
+}
+
 #[test]
-fn it_works() {
-    assert_eq!(2 + 2, 4);
+fn validate_parser_should_parse_char_match() {
+    let seed_vec = vec!['a', 'b', 'c'];
+
+    assert_eq!(Ok((&seed_vec[1..], 'a')), match_char('a').parse(&seed_vec));
+}
+
+#[test]
+fn validate_parser_can_map_a_result() {
+    let seed_vec = vec!['a', 'b', 'c'];
+
+    assert_eq!(
+        Ok((&seed_vec[1..], 'a'.to_string())),
+        map(match_char('a'), |result| { result.to_string() }).parse(&seed_vec)
+    );
 }
